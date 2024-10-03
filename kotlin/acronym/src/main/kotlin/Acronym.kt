109dbddfb16fef557def866a22627937c482f13e
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2024-10-03 17:29:52 +0100

    refactor acronym

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,5 +1,4 @@
 object Acronym {
-    private val regex = """(\w+)""".toRegex()
     fun generate(phrase: String) : String {
         val words = phrase.split(" ", "-").filterNot { it.isEmpty() }.map { it.replace("_", "") }
         return words.joinToString("") { it.first().uppercase() }
